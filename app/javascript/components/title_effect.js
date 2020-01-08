342ceaf53c7c515dc7a6128c07e3f3ab26bb0c94
Leo Chaput <leo.chaput@hotmail.fr>
2020-01-08 20:10:30 +0100

    modifications on images for project page and some front end

--- app/javascript/components/title_effect.js
+++ app/javascript/components/title_effect.js
@@ -1,68 +1,20 @@
 const titleEffect = () => {
-  if (document.getElementById('title')) {
-    const title = document.getElementById('title').innerText;
-    const subhead = document.getElementById('subhead').innerText;
-    const description = document.getElementById('description').innerText;
+  if (document.querySelector('.repeat')) {
+    window.onload = function(){
+      var tl = new TimelineLite({delay: 1}),
+        firstBg = document.querySelectorAll('.text__first-bg'),
+        secBg = document.querySelectorAll('.text__second-bg'),
+        word  = document.querySelectorAll('.text__word');
 
-    // this function turns a string into an array
-    const createLetterArray = (string) => {
-      return string.split('');
+      tl
+        .to(firstBg, 0.2, {scaleX:1})
+        .to(secBg, 0.2, {scaleX:1})
+        .to(word, 0.1, {opacity:1}, "-=0.1")
+        .to(firstBg, 0.2, {scaleX:0})
+        .to(secBg, 0.2, {scaleX:0});
+
+      document.querySelector('.repeat').onclick = function() {tl.restart()};
     }
-
-    // this function creates letter layers wrapped in span tags
-    const createLetterLayers = (array) => {
-      return array.map((letter) => {
-          let layer = '';
-          //specify # of layers per letter
-          for (let i = 1; i <= 2; i++) {
-            // if letter is a space
-            if(letter == ' '){
-              layer += '<span class="space"></span>';
-            }else{
-              layer += '<span class="letter-'+i+'">'+letter+'</span>';
-            }
-          }
-          return layer;
-      });
-    }
-
-    // this function wraps each letter in a parent container
-    const createLetterContainers = (array) => {
-      return array.map((item) => {
-        let container = '';
-        container += '<div class="wrapper">'+item+'</div>';
-        return container;
-      });
-    }
-
-    // use a promise to output text layers into DOM first
-    const outputLayers = new Promise(function(resolve, reject) {
-          document.getElementById('title').innerHTML = createLetterContainers(createLetterLayers(createLetterArray(title))).join('');
-          document.getElementById('subhead').innerHTML = createLetterContainers(createLetterLayers(createLetterArray(subhead))).join('');
-          document.getElementById('description').innerHTML = createLetterContainers(createLetterLayers(createLetterArray(description))).join('');
-
-          resolve();
-    });
-
-    // then adjust width and height of each letter
-    const spans = Array.prototype.slice.call(document.getElementsByTagName('span'));
-    outputLayers.then(() => {
-        return spans.map((span) => {
-          setTimeout(() => {
-            span.parentElement.style.width = span.offsetWidth+'px';
-            span.parentElement.style.height = span.offsetHeight+'px';
-          }, 250);
-        });
-    }).then(() => {
-        // then slide letters into view one at a time
-        let time = 250;
-        return spans.map((span) => {
-          time += 25;
-          setTimeout(() => {
-            span.parentElement.style.top = '0px';
-          }, time);
-        });
-    });
   };
 }
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,4 @@ window.functionDisplay = functionDisplay
 functionColorChange();
 navbarUnderline();
 aosEffect();
+titleEffect();
